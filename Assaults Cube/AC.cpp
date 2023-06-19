0fa2ff2d4e3483c0192d13dc79cb38eca882a051
Yassine <Yassine.bousselmi23@gmail.com>
2023-06-19 13:09:02 +0100

    updated offsets

--- Assaults Cube/AC.cpp
+++ Assaults Cube/AC.cpp
@@ -38,13 +38,12 @@ uintptr_t GetModuleBaseAddr(DWORD pid, const wchar_t* ModName) {
 	}
 }
 
-uintptr_t FindDMAAddry(HANDLE hproc, uintptr_t ptr, std::vector<unsigned int> offsets){
+uintptr_t FindDMAAddry(HANDLE handp, uintptr_t ptr, std::vector<unsigned int> off){
 	uintptr_t addr = ptr;
-	for (int i = 0; i < offsets.size(); i++) {
-		ReadProcessMemory(hproc, (BYTE*)addr, &addr, sizeof(addr), nullptr);
-		addr += offsets[i];
+	for (int i = 0; i < off.size(); i++) {
+		ReadProcessMemory(handp, (BYTE*)addr, &addr, 4, nullptr);
+		addr += off[i];
 	}
-
 	return addr;
 }
 
@@ -55,20 +54,29 @@ int main() {
 
 	HANDLE hproc = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
 
-	uintptr_t lplayerbaseaddr = modbaseaddr + 0x10F4F4;
-	
-	uintptr_t ammooffset = lplayerbaseaddr + 0x0150;
+	uintptr_t lplayerbaseaddr = modbaseaddr + 0x195404;
 
-	/*OFFSETS WRONG*//*OFFSETS WRONG*//*OFFSETS WRONG*/
+	std::vector<unsigned int> offsets = { 0x0374 , 0x014 , 0x0 };
+	uintptr_t ammooffset = FindDMAAddry(hproc, lplayerbaseaddr, offsets);
+	
+
 	
 	DWORD ammo;
-	ReadProcessMemory(hproc,(BYTE*)ammooffset, &ammo, sizeof(ammo), nullptr);
-	std::cout << "Current ammo " << ammo;
+	ReadProcessMemory(hproc, (LPCVOID*)ammooffset , &ammo, sizeof(ammo), nullptr);
+	std::cout << "ammo adress " << std::hex << ammooffset << std::endl;
+	std::cout << "Current ammo " << std::dec << ammo << std::endl;
+	
 
 	DWORD newammo = 30;
+	DWORD infammo = 9999;
+	std::cout << "Press F5 to reset ammo to 30" << std::endl;
+	std::cout << "Press F6 to get infinite ammo" << std::endl;
 	while (true) {
 		if (GetAsyncKeyState(VK_F5)!=0){
-			WriteProcessMemory(hproc, (BYTE*)ammooffset, &newammo, sizeof(newammo), nullptr);
+			WriteProcessMemory(hproc, (LPVOID*)ammooffset, &newammo, sizeof(newammo), nullptr);
+		}
+		if (GetAsyncKeyState(VK_F6) != 0) {
+			WriteProcessMemory(hproc, (LPVOID*)ammooffset, &infammo, sizeof(infammo), nullptr);
 		}
 	}
 
